6a4ec19069fc042d6a64d97401121f10f813d353
catherini168 <catherini168@gmail.com>
2019-08-17 17:28:57 +0000

    view-all-grades

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import Header from './header';
+
+import GradeTable from './gradetable';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grade: []
+
+    };
+  }
+  componentDidMount() {
+    fetch(`/api/grades`)
+      .then(response => response.json())
+      .then(data => this.setState({ grade: data }));
+  }
+
   render() {
-    return null;
+    return (
+      <div>
+        <Header></Header>
+        <GradeTable grade= {this.state.grade}/>
+
+      </div>
+    );
   }
 }
 
